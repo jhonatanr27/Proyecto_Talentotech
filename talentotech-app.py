0533dc14189257425609ae69ed29b1938b8409f3
jhonatanr27 <jhonatan.ramirezc@gmail.com>
2025-02-17 07:21:43 -0500

    Update talentotech-app.py

---
+++ talentotech-app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from datetime import datetime
+
+# 1. configuración inicial de la aplicación
+
+st.set_page_config(
+   page_litle="Dashboard Interactivo",
+   page_icon= "♫",
+   layout="wide"
+)
+st.title("♫"Dashboard Interactivo con Streamlit")
+st.sidebar.title("  Opciones de Navegacion"
